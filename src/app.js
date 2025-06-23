223ee0a4502d1f6c3a19358e10b77f0d4a6677bf
Nadev <120123131+Nadee23@users.noreply.github.com>
2025-06-23 10:34:34 +0000

    done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our', 'your', 'my'];
+  let adj = ['great', 'big', 'awesome', 'funny', 'despicable'];
+  let noun = ['jogger', 'racoon', 'rainbow', 'coach', 'beef'];
+  let extensions = ['.com', '.net', '.org', '.es', '.fr'];
+
+
+    for (let i = 0; i < pronoun.length; i++) {
+        for (let j = 0; j < adj.length; j++) {
+            for (let k = 0; k < noun.length; k++) {
+                for (let l = 0; l < extensions.length; l++) {
+                    console.log(pronoun[i] + adj[j] + noun[k] + extensions[l])
+                }
+            }
+        }
+    }
+}
